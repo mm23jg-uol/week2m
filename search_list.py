554288de25fc13fdc7855759b670a870b0483025
YiranHu8628 <hnth8628@leeds.ac.uk>
2025-02-06 16:31:57 +0000

    Create search_list.py

---
+++ search_list.py
@@ -0,0 +1,14 @@
+search_ordered_list(lst, target):
+    sorted_lst = get_ordered_list(lst)
+    low = 0
+    high = length of sorted_lst - 1
+     low <= high DO:
+        mid = (low + high) // 2
+        IF sorted_lst[mid] equals target THEN:
+            RETURN True
+        ELSE IF sorted_lst[mid] < target THEN:
+             low = mid + 1
+        ELSE:
+             high = mid - 1
+   
+    RETURN False
